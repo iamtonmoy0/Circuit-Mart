ef2d87ff5af32c7e7ecd80393ab05a89d991f40a
iamtonmoy0 <tonmoy2code@gmail.com>
2023-08-29 06:49:02 -0400

    feat:basic server started.

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const init = () => {
+    let app = (0, express_1.default)();
+    app.get('/', (req, res) => {
+        res.send('hello world');
+    });
+    return app;
+};
+const app = init();
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import express ,{Application} from 'express';
+
+const init=():Application=>{
+let app = express();
+
+app.get('/',(req,res)=>{
+	res.send('hello world')
+})
+
+return app;
+}
+
+const app = init();
+app.listen(3000,()=>{
+	console.log('Listening on port 3000');
+})
