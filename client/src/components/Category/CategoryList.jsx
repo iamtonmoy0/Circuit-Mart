273479d88c9c05e935550c11a12fae710fa5f4d5
iamtonmoy0 <tonmoy2code@gmail.com>
2023-09-22 10:30:46 -0400

    feat: category tag mobile device responsive added

--- client/src/components/Category/CategoryList.jsx
+++ client/src/components/Category/CategoryList.jsx
@@ -1,25 +1,58 @@
-import { useEffect, useState } from "react"
-import { getCategories } from "../../functions/categoryFunctions"
+import { useEffect, useState } from "react";
+import { getCategories } from "../../functions/categoryFunctions";
 
 const CategoryList = () => {
-	const [categories,setCategories]=useState([])
+  const [categories, setCategories] = useState([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
- //render load categories
- useEffect(()=>{
-    loadCategories()
-  },[])
-  // load categories
-const loadCategories=()=>{
-getCategories()
-.then((res)=>setCategories(res.data.data))
-}
-	return (
-		<div className="py-3 px-5 bg-[#F3FDE8]">
-			<div className="flex cursor-pointer  ">
-			{categories && categories.length>0 && categories.map(cat=><p  style={{fontWeight:'80px'}} className="ml-2  hover:bg-green-300 py-1 rounded"  key={cat._id}> {cat.name} </p>)}
-			</div>
-		</div>
-	);
-}
+  // Render load categories
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  // Load categories
+  const loadCategories = () => {
+    getCategories().then((res) => setCategories(res.data.data));
+  };
+
+  // Toggle the menu open/closed
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="relative">
+      <div className="py-3 px-5 bg-[#F3FDE8]">
+        <div className="flex cursor-pointer flex-wrap">
+          {categories && categories.length > 0 && (
+            <>
+              {categories.map((cat, index) => (
+                <p
+                  key={cat._id}
+                  className={`${
+                    isMenuOpen ? "mb-1" : index !== 0 ? "ml-2" : ""
+                  } hover:bg-green-300 py-1 rounded`}
+                >
+                  {cat.name}
+                </p>
+              ))}
+              {/* Show the "Show More" button inline with categories on mobile and tablet */}
+              {categories.length > 15 && (
+                <button
+                  className={`text-gray-700 hover:text-gray-900 pl-5  ${
+                    isMenuOpen ? "mt-1 ml-2" : "mt-2"
+                  }`}
+                  onClick={toggleMenu}
+                >
+                  {isMenuOpen ? "Show Less" : "Show More"}
+                </button>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CategoryList;
