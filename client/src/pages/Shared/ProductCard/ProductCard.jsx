e1705da2ec504dab727441939371e769fc657522
iamtonmoy0 <tonmoy2code@gmail.com>
2023-10-01 09:14:28 -0400

    feat: add to cart functionality added to local storage

--- client/src/pages/Shared/ProductCard/ProductCard.jsx
+++ client/src/pages/Shared/ProductCard/ProductCard.jsx
@@ -4,9 +4,35 @@ import { Link } from "react-router-dom";
 import * as routePath from '../../../routes/routePath';
 import StarRating from "../StarRating/StarRating";
 
-const ProductCard = (product) => {
+const ProductCard = ({product}) => {
 	// console.log(product)
-	const{title,price,images,description,slug,ratings} = product.product
+	const{title,price,images,description,slug,ratings,_id} = product;
+
+
+// handle cart
+const handleAddToCart = ()=>{
+  // Check if the product is already in the cart
+  let cart = [];
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('cart')) {
+      cart = JSON.parse(localStorage.getItem('cart'));
+    }
+    // Check if the product is already in the cart by comparing product IDs
+    const existingProduct = cart.find((item) => item._id === product._id);
+
+    if (existingProduct) {
+      // If the product exists in the cart, you can update its quantity or take other actions
+      // For example, you can increment the quantity of the existing product
+      existingProduct.count += 1;
+    } else {
+      // If the product is not in the cart, add it
+      cart.push({ ...product, count: 1 });
+    }
+
+    // Save the updated cart back to local storage
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+}
 	return (
 		
 <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -22,7 +48,7 @@ const ProductCard = (product) => {
         </div>
         <div className="flex items-center mt-2.5 mb-5">
             {/* ratings */}
-				{product && ratings && ratings.length > 0 ? <StarRating product={product.product} /> : "No ratings yet"}
+				{product && ratings && ratings.length > 0 ? <StarRating product={product} /> : "No ratings yet"}
 			
             </div>
         <div className="flex items-center justify-between">
@@ -41,9 +67,9 @@ const ProductCard = (product) => {
     className="text-center cursor-pointer text-sm text-green-700 dark:text-gray-200"
   
   >
-    <div className="flex flex-col items-center">
+    <div onClick={handleAddToCart} className="flex flex-col items-center">
       <BsFillCartPlusFill className="text-2xl" />
-      <p>Add to cart</p>
+      <a>Add to cart</a>
     </div>
   </button>
 </div>
